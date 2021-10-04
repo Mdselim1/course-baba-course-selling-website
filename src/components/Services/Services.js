bf152128dca1c1febc9de90697c75a1e216865ba
MDSELIM1 <selimakodo58@gmail.com>
2021-10-04 21:04:45 +0600

    add

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,5 +1,7 @@
+import { Button } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import UiData from '../../hooks/uidata';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
@@ -20,7 +22,13 @@ const Services = () => {
                 data.map(uidata => <ServiceDisData key={uidata.key} data={uidata}></ServiceDisData>)
                 }
                 </Box>
-                
+                <div style={{textAlign:'center'}}>
+                    <Link style={{
+                        textDecoration:'none'
+                    }} to="/services">
+                    <Button sx={{mt:'20px' , mb:'40px' , py:'10px',px:'30px' ,bgcolor:'#F7B213' , color:'#16364D', fontSize:'20px', fontWeight:'600'}} variant="contained">Upcoming Courses</Button>
+                </Link>
+                </div>
             </div>
             <Footer></Footer>
         </div>
