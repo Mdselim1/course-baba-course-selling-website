14044f2e50076e89bc942a9fe3bf8ddbaa84d833
MDSELIM1 <selimakodo58@gmail.com>
2021-10-05 16:23:54 +0600

    add

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,16 +1,22 @@
 import { Button } from '@mui/material';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 import './Notfound.css';
 
 const NotFound = () => {
     return (
-        <div className="not_found_page">
+        <div>
+            <Header></Header>
+            <div className="not_found_page">
             <h1>4<span>0</span>4</h1>
             <h2>Page not Found</h2>
             <Link style={{textDecoration:'none'}} to="/home">
             <Button sx={{my:'10px'}} variant="contained">Back to home page</Button>
             </Link>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
